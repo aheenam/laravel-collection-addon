193d6e9130d1142d0dcb0ea9e6bbca04b34a4476
Rathes Sachchithananthan <rs@net-insiders.com>
2018-02-19 12:56:31 +0100

    Initial commit

---
+++ src/LaravelCollectionAddonServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Aheenam\LaravelCollectionAddon;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCollectionAddonServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
